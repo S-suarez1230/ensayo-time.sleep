68c411f24fced6f96561b44f7e20e62b3abc79a3
S-suarez1230 <s.suarez.r1230@gmail.com>
2025-03-20 23:14:31 +0100

    Add files via upload

---
+++ ensayo-time.sleep.py
@@ -0,0 +1,7 @@
+import time
+contador = 0
+while contador <= 9:
+    contador = contador + 1
+    print(contador)
+    time.sleep(3)
+#es un bucle con una pausa, importando la extension de time de la biblioteca, la cual hace que haya un tiempo de espera antes del siguiente codigo
